f93fc2ba28fb532a1cc6900e9edf7518c8bf4047
Ruoqian Zhang <31377103+voidii@users.noreply.github.com>
2019-01-08 13:51:32 -0800

    Add files via upload

---
+++ C hash/HelloWorld.cs
@@ -0,0 +1,12 @@
+using System;
+namespace HelloWorldApplication
+{
+	class HelloWorldApplication{
+		static void Main(string[] args)
+		{
+			//my first C# program
+			Console.WriteLine("Hello World");
+			Console.ReadKey();
+		}
+	}
+}
